fe304419eca88c8dac5aad6c95731acfeef69072
Шмыгленко Никита <99179542+Shmygam@users.noreply.github.com>
2022-03-13 14:04:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  uint64_t low = 0, up = (size - 1), count = 0;
+
+    while (low < up) {
+        uint64_t key = (low + up) / 2;
+        if (arr[key] == value) {
+
+            uint64_t temp = key;
+
+            while (arr[key -= 1] == value) {
+                count += 1;
+            }
+            while (arr[temp += 1] == value) {
+                count += 1;
+            }
+
+            return count + 1;
+        }
+
+        else if (arr[key] > value) {
+            up = key;
+        }
+        else {
+            low = key + 1;
+        }
+    }
+    return false;
 }
